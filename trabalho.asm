2bf2f2e87f6a41372ae7be0be7167a86e6b79576
Gabriel Malosto <gabriel.malosto@estudante.ufjf.br>
2023-05-12 03:40:06 -0300

    Inicia função inicializaVetor

--- trabalho.asm
+++ trabalho.asm
@@ -1,59 +1,26 @@
-.data
-	# Área para dados na memória principal
-	#
-	
-	size: 80	# Tamanho do vetor em bytes (20 espaços de 4 bytes)
-	
-	vetor:	# Declaração do vetor
+.data	# Área para dados na memória principal
+	size: 20	# Tamanho do vetor em número de posições
+	vet:	# Declaração do vetor
 		.align 2	# Tamanho de cada elemento
 		.space 80	# Quantidade de bytes de espaço alocados (20 espaços de 4 bytes)
 
-.text
-	# Área para instruções do programa
-	#
-	#
-	#
-	
+.text	# Área para instruções do programa
 
-		
-	
-	# Inicializa vetor com número pseudo-aleatórios
-	# Param: $a0 - Endereço base do vetor; $a1 - Tamanho do vetor em bytes; $a2 - Último valor alteatório utilizado
-#	inicializaVetor:
-	# Prólogo
-#		addi $sp, $sp, -0	# Ajusta a pilha
-#		sw $a0, 0($sp)
-#		sw $a1, 0($sp)
-#		sw $a2, 0($sp)
-#		sw $ra, 0($sp)
-		
-		# Loop
-#		inicializaVetorLaco1:
-#			move $t0, $v0
-		
-		# Epílogo
-#		inicializaVetorEpilogo:
-#			lw $a0, 0($sp)
-		
 	# Programa principal
 	.main
-#		move $t0, $zero	# Índice do vetor
-#		move $t1, $zero	# Valor a ser colocado no vetor
-#		lw $t2, size	# Tamanho do vetor em bytes
-		
 		# Prólogo
 		#
 		addi $sp, $sp, -20
+		la $s0, vet		# Endereço do vetor
+		lw $s1, size	# sizeTamanho do vetor em bytes
 		
 		# Corpo
 		#
-		addi $a0, $zero, 10
-		addi $a1, $zero, 2
-		addi $a2, $zero, 4
-		addi $a3, $zero, 11
-		addi $t0, $zero, 2
-		sw $t0, 16($sp)	# e = 1; guarda argumento na memória
-		jal valorAleatorio
+		# Chama inicializaVetor
+		move $a0, $s0		# Arg: vetor = vet
+		move $a1, $s1		# Arg: tamanho = size
+		addi $a2, $zero, 71	# Arg: ultimoValor = 71
+		jal inicializaVetor
 		
 		move $a0, $v0
 		li $v0, 1
@@ -64,18 +31,18 @@
 		addi $sp, $sp, 20
 		li $v0, 10	# Instrução: encerra o programa
 		syscall
-	
+	# ========== #
 	
 	# Funções
 	#
 	#
 		
 	# Gera número pseudo-aleatório por congruência linear
-	# Param: $a0 - 1o inteiro; $a1 - 2o inteiro; $a2 - 3o inteiro; $a3 - 4o inteiro
+	# Param: int a - 1o inteiro; int b - 2o inteiro; int c - 3o inteiro; int d - 4o inteiro
 	valorAleatorio:
 		# Prólogo
 		#
-		lw $t0, 16($sp)	# Recupera o 5o parâmetro
+		lw $t0, 16($sp)	# Recupera o parâmetro int e
 		
 		# Corpo
 		#
@@ -89,4 +56,48 @@
 		# Epílogo
 		#
 		move $v0, $t1	# return aux
-		jr $ra
+		jr $ra	
+	# ---------- #
+	
+	
+	# Inicializa vetor com número pseudo-aleatórios
+	# Param: int vetor[] - Endereço base do vetor; int tamanho - Tamanho do vetor em bytes; int ultimoValor - Último valor alteatório utilizado
+	inicializaVetor:
+		# Prólogo
+		#
+		addi $sp, $sp, -20	# Ajusta a pilha
+		sw $a0, 0($sp)		# Salva arg: vetor
+		sw $a1, 4($sp)		# Salva arg: tamanho
+		sw $a2, 8($sp)		# Salva arg: ultimoValor
+		sw $ra, 12($sp)		# Salva endereço de retorno
+		
+		# Corpo
+		#
+		inicializaVetorLaco:
+			ble  $a1, $zero, inicializaVetorCasoBase	# if (tamanho <= 0)... chama caso base
+			# tamanho > 0...
+			# Chama valorAleatorio
+			move $a0, $a2			# Arg: a = ultimoValor
+			addi $a1, $zero, 47		# Arg: b = 47
+			addi $a2, $zero, 97		# Arg: c = 97
+			addi $a3, $zero, 337	# Arg: d = 337
+			addi $t0, $zero, 3		# Arg: e = 3
+			sw $t0, 16($sp)			#
+			jal valorAleatorio
+			#move $t0, $v0			# aux = valorAleatorio()
+			j inicializaVetorEpilogo
+		
+		inicializaVetorCasoBase:
+			move $v0, $zero	# return 0
+		
+		# Epílogo
+		#
+		inicializaVetorEpilogo:
+			lw $a0, 0($sp)		# Recupera arg: vetor
+			lw $a1, 4($sp)		# Recupera arg: tamanho
+			lw $a2, 8($sp)		# Recupera arg: ultimoValor
+			lw $ra, 12($sp)		# Recupera endereço de retorno
+			addi $sp, $sp, 20	# Reajusta a pilha
+			jr $ra
+	# ---------- #
+	
