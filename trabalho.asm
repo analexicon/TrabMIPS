6c9d57f64a8dfc83135886901bf65b639007c8a7
Gabriel Malosto <gabriel.malosto@estudante.ufjf.br>
2023-05-12 02:29:12 -0300

    Finaliza função valorAleatorio

--- trabalho.asm
+++ trabalho.asm
@@ -11,6 +11,8 @@
 .text
 	# Área para instruções do programa
 	#
+	#
+	#
 	
 
 		
@@ -33,34 +35,58 @@
 #		inicializaVetorEpilogo:
 #			lw $a0, 0($sp)
 		
-	
-	.main	# Programa principal
+	# Programa principal
+	.main
 #		move $t0, $zero	# Índice do vetor
 #		move $t1, $zero	# Valor a ser colocado no vetor
 #		lw $t2, size	# Tamanho do vetor em bytes
 		
+		# Prólogo
+		#
+		addi $sp, $sp, -20
+		
+		# Corpo
+		#
 		addi $a0, $zero, 10
-		addi $a1, $zero, 4
+		addi $a1, $zero, 2
+		addi $a2, $zero, 4
+		addi $a3, $zero, 11
+		addi $t0, $zero, 2
+		sw $t0, 16($sp)	# e = 1; guarda argumento na memória
 		jal valorAleatorio
 		
 		move $a0, $v0
 		li $v0, 1
 		syscall
-		
 	
-		foraDaImpressao:
-			li $v0, 10	# Instrução: encerra o programa
-			syscall
+		# Epílogo
+		#
+		addi $sp, $sp, 20
+		li $v0, 10	# Instrução: encerra o programa
+		syscall
 	
 	
 	# Funções
+	#
 	#
 		
 	# Gera número pseudo-aleatório por congruência linear
 	# Param: $a0 - 1o inteiro; $a1 - 2o inteiro; $a2 - 3o inteiro; $a3 - 4o inteiro
 	valorAleatorio:
-		#lw $t0, 16($sp)	# $t0 recebe o 5o argumento
-		mult $a0, $a1	# a * b
+		# Prólogo
+		#
+		lw $t0, 16($sp)	# Recupera o 5o parâmetro
+		
+		# Corpo
+		#
+		mult $a0, $a1	# aux = a * b
 		mflo $t1
-		move $v0, $t1
+		add $t1, $t1, $a2	# aux = aux + c
+		div $t1, $a3	# aux = aux % d
+		mfhi $t1
+		sub $t1, $t1, $t0	# aux = aux - e
+		
+		# Epílogo
+		#
+		move $v0, $t1	# return aux
 		jr $ra
